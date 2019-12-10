93e5d480d553e942df10d1511d50a7493489de86
cdr <1254688559@qq.com>
2019-12-10 22:50:28 +0800

    整型提升练习题

---
+++ 整型提升/practice.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#if 0
+int main(){
+	char a = -1;
+	signed char =-1;
+	unsigned char = -1;
+	printf("a = %d ,b =%d ,c = %d \n",a,b,c);
+	return 0;
+}
+#endif
+int main(){
+	//char a = -128;
+	char a =128;
+	//printf("%d\n",a);
+	printf("%u\n",a);
+	return 0;	
+}

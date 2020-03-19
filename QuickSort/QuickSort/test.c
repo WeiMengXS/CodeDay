1adc9268229c9e1c3423faf85e820c024cf3d7ee
Wmxs <1254688559@qq.com>
2020-03-19 22:14:43 +0800

    QuickSort

--- QuickSort/QuickSort/test.c
+++ QuickSort/QuickSort/test.c
@@ -1,4 +1,6 @@
 #include<stdio.h>
+#include<stdlib.h>
+
 void Swap(int *a, int *b)
 {
 	int tmp = 0;
@@ -7,9 +9,35 @@ void Swap(int *a, int *b)
 	*b = tmp;
 
 }
-int PartSort(int *a, int begin,int end)
+int indexnum(int *a, int left, int right)
 {
 
+	int mid = (left + right)/2;
+	if (a[right] > a[mid])
+	{
+		if (a[mid] > a[left])
+			return mid;
+		else if (a[right] > a[left])
+			return left;
+		else return right;
+	}
+	else
+	{
+		if (a[mid] > a[right])
+			return mid;
+		else if (a[left] > a[right])
+			return right;
+		else return left;
+		
+		
+
+	}
+
+}
+int PartSort(int *a, int begin,int end)
+{
+	//int x = indexnum(a,begin,end);
+	//Swap(&a[x],&a[end]);
 	int key = end;
 
 
@@ -44,15 +72,56 @@ void QuickSort(int *a, int left, int right)
 
 
 }
+void CountSort(int* a, int n)
+{
+	// 1、计算出统计次数的数组的大小
+	int max = a[0];
+	int min = a[0];
+	for (int i = 0; i < n; ++i)
+	{
+		if (a[i] > max)
+			max = a[i];
+
+		if (a[i] < min)
+			min = a[i];
+	}
+
+	int range = max - min + 1;
+	int* countA = (int*)malloc(sizeof(int)*range);
+
+	// 1、统计数据出现的次数
+
+	for (int i = 0; i < range; i++)
+	{
+		countA[i] = 0;
+	}
+	// 2、根据次数排序，写回原数组
+	for (int i = 0; i < 0; i++)
+	{
+		int count = a[i] - min;
+		countA[count]++;
+	}
+
+	int index = 0;
+	for (int i = 0; i < range; i++)
+	{
+		while (countA[i]--)
+		{
+			a[index++] = i + min;
+		}
+
+	}
+	free(countA);
+}
 int main()
 {
-	int a[10] = {5,4,9,8,3,2,4,1,0,7};
+	int a[10] = {8,1,2,7,9,3,4,5,10,6};
 
 	//PartSort(a, 0, sizeof(a)/sizeof(a+0)-1);
-	QuickSort(a, 0, sizeof(a) / sizeof(a + 0) - 1);
+	//QuickSort(a, 0, sizeof(a) / sizeof(a + 0) - 1);
 
+	CountSort(a, 10);
 
-
-
+	system("pause");
 	return 0;
 }
